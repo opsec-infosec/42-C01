92a9899dcd04f71638b61f9d1c334833d4add700
Dale Furneaux <dfurneau@lab1r1s12.42abudhabi.ae>
2021-05-26 16:56:18 +0400

    Fixed Ex08 Fixed error with calling function name, change sort order in array

--- ex08.c
+++ ex08.c
@@ -6,15 +6,15 @@ void ft_sort_int_tab(int *tab, int size);
 
 int main(void) {
 	
-	int a[5] = {1,2,3,4,5};
+	int a[5] = {2,3,1,5,4};
 	int i = 0;
 	int size = (sizeof(a)/sizeof(int));
 
 	printf("a = ");
 	for (i = 0;i < size;i++)
 		printf("%d ",*(a+i));
-	printf("\nCalling ft_sort_int_tab(a, 5);\n"); 
-	ft_rev_int_tab(a, size);
+	printf("\nCalling ft_sort_int_tab(a, 5);\n");
+	ft_sort_int_tab(a, size);
 	printf("a = ");
 	for (i = 0;i < size;i++)	
 		printf("%d ",*(a+i));
